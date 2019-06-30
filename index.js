d486eb0418ff37e2bf7fe99099466a9f104dd7ae
frige123 <chopkake@gmail.com>
2019-06-30 16:06:48 +0300

    hello

---
+++ index.js
@@ -0,0 +1,82 @@
+function myfunc(value){
+    if(value >=0 && value <=7){
+        alert('big')
+    };
+    if(value >=7 && value <=12){
+        alert('favourite')
+    };
+    if(value >=12 && value <=18){
+        alert('teenagers')
+    }
+    if(value >=18 && value <=100){
+        alert('oldest pupil')
+    }
+    if(value >0 || value <100){
+        alert('no corect value')
+    }
+     }
+alert('hello')
+/////homework/////
+let str = 'Привет'
+let num = 123;
+let flag = true;
+let txt = 'true;'
+typeof (str);
+console.log('str',typeof(str)); 
+typeof(num);
+console.log('num',typeof(num));
+typeof(flag);
+console.log('flag',typeof(flag));
+typeof(txt);
+console.log('txt',typeof(txt));
+let a1;
+let a2;
+let a3;
+let a4;
+let a5;
+a1 = 5 +3;  
+console.log('a1',a1);
+a2 = 5 - 3;
+console.log('a2',a2);
+a3 = 5 * 3;
+console.log('a3',a3);
+a4 = 5 / 3;
+console.log('a4',a4);
+a5 = 6 + 7;
+console.log('a5',a5);
+let a6;
+let a7;
+let a8;
+let a9;
+let a10;
+a6 = 5 % 3;
+console.log('a6',a6);
+a7 = 3 % 5;
+console.log('a7',a7);
+a8 = 5 + '3';
+console.log('a8',a8);
+a9 = '5' + 3;
+console.log('a9',a9);
+a10 = 75+ 'кг';
+console.log('a10',a10);
+let height = 23;
+let width = 10;
+let s ;
+s = height * width;
+console.log('s',s);
+let heightC = 10;
+let dC = 4;
+let v;
+let p = 3.14;
+v = heightC * (p*dC*dC/4);
+console.log('v',v)
+let nFib = 12.
+let t;
+t = Math.pow(12,12);
+console.log('t',t);
+let m = 4;
+let n = 3;
+let k  ;
+k = (Math.pow(m,2)) + (Math.pow(n,2));
+k = Math.sqrt(k);
+console.log('k',k)
